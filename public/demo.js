b89ca8f679679238ef91ab261b4df5004fdadd33
Doug S <doug.sillars@gmail.com>
2020-05-05 06:29:03 +0200

    working demo

---
+++ public/demo.js
@@ -0,0 +1,240 @@
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+const live = urlParams.get('live');
+console.log("live?",live);
+var framerate = 15;
+var audioBitrate = 11025;
+var width = 240;
+var height = 240;
+
+if(live){
+	
+	var mediaRecorder;
+ 	//var socket =io();
+ 	var socket;
+	
+	var state ="stop";
+	console.log("state initiated = " +state); 
+	connect_server();
+
+	
+	//do live stuff
+}
+ 
+ 
+ 
+ function thisFileUpload() {
+    //get VOD video to upload
+	 const fileElement = document.getElementById('file');
+	 
+	 fileElement.click();
+	//upload file to NodeJS for upload to api.video
+	fileElement.addEventListener("change", function() {	
+			 console.log("file selected", document.getElementById('file').files[0]);
+			 uploadForm.requestSubmit();
+		});
+    //send to Node server to uploa
+	console.log('done');
+};
+
+ function initiateLivestream() {
+	 //set up livestream
+	 livestreamForm.requestSubmit();
+	
+  }
+
+
+
+
+function video_show(stream){
+	console.log("videoshow");
+	//console.log("output_video", output_video);
+	if ("srcObject" in output_video) {
+	//	console.log("videoshowif");
+		output_video.muted = true;
+		output_video.srcObject = stream;
+		
+	} else {
+	//	console.log("videoshowelse");
+		output_video.src = window.URL.createObjectURL(stream);
+		
+	}
+  output_video.addEventListener( "loadedmetadata", function (e) {
+	  console.log("outputvideoevent listener");
+  		console.log(output_video);
+		output_message.innerHTML="Local video source size:"+output_video.videoWidth+"x"+output_video.videoHeight ;
+	}, false );
+	//console.log("output_video", output_video);
+}
+
+function show_output(str){
+	output_console+="\n"+str;
+	console.log("response data",str);
+	output_console.scrollTop = output_console.scrollHeight;
+};
+
+	function connect_server(){
+
+		var socketio_address = "/";
+		console.log("connect server started");
+		navigator.getUserMedia = (navigator.mediaDevices.getUserMedia ||
+                          navigator.mediaDevices.mozGetUserMedia ||
+                          navigator.mediaDevices.msGetUserMedia ||
+                          navigator.mediaDevices.webkitGetUserMedia);
+		if(!navigator.getUserMedia){fail('No getUserMedia() available.');}
+		if(!MediaRecorder){fail('No MediaRecorder available.');}
+        
+		var socketOptions = {secure: true, reconnection: true, reconnectionDelay: 1000, timeout:15000, pingTimeout: 			15000, pingInterval: 45000,query: {framespersecond: framerate, audioBitrate: audioBitrate}};
+		
+		//start socket connection
+		console.log("socket address", "/");
+		socket = io.connect("/", socketOptions);
+		console.log("socket", socket);
+		// console.log("ping interval =", socket.pingInterval, " ping TimeOut" = socket.pingTimeout);
+ 		//output_message.innerHTML=socket;
+		
+		socket.on('connect_timeout', (timeout) => {
+   			console.log("state on connection timeout= " +timeout);
+			output_message.innerHTML="Connection timed out";
+			//recordingCircle.style.fill='gray';
+			
+		});
+		socket.on('error', (error) => {
+   			console.log("state on connection error= " +error);
+			output_message.innerHTML="Connection error";
+		//	recordingCircle.style.fill='gray';
+		});
+		
+		socket.on('connect_error', function(){ 
+   			console.log("state on connection error= " +state);
+			console.log("Connection Failed");
+			output_message.innerHTML="Connection Failed";
+		//	recordingCircle.style.fill='gray';
+		});
+
+		socket.on('message',function(m){
+			console.log("state on message= " +state);
+			console.log('recv server message',m);
+			output_message.innerHTML+=('SERVER:'+m);
+			
+		});
+
+		socket.on('fatal',function(m){
+
+			output_message.innerHTML+=('Fatal ERROR: unexpected:'+m);
+			//alert('Error:'+m);
+			console.log("fatal socket error!!", m);
+			console.log("state on fatal error= " +state);
+			//already stopped and inactive
+			console.log('media recorder restarted');
+			//recordingCircle.style.fill='gray';
+			
+			//mediaRecorder.start();
+			//state="stop";
+			//restart the server
+	
+		
+			//should reload?
+		});
+		
+		socket.on('ffmpeg_stderr',function(m){
+			//this is the ffmpeg output for each frame
+			output_message.innerHTML+=('FFMPEG:'+m);	
+		});
+
+		socket.on('disconnect', function (reason) {
+			console.log("state disconec= " +state);
+			output_message.innerHTML+=('ERROR: server disconnected!');
+			console.log('ERROR: server disconnected!' +reason);
+			//recordingCircle.style.fill='gray';
+			//reconnect the server
+			//connect_server();
+		
+	
+		});
+	
+		state="ready";
+		console.log("connected state = " +state);
+		console.log("connect server successful");
+	//	output_message.innerHTML="connect server successful";
+		requestMedia();
+}
+function requestMedia(){
+/*	var audioBitrate = 11025;
+	var width = 240;
+	var height = 240;
+	var framerate = 15;
+	*/
+	console.log("request media");
+	var constraints = { audio: {sampleRate: audioBitrate},
+		video:{
+	        width: { min: 100, ideal: width, max: 1920 },
+	        height: { min: 100, ideal: height, max: 1080 },
+			frameRate: {ideal: framerate}
+	    }
+	};
+	console.log(constraints);
+	navigator.getUserMedia = (navigator.mediaDevices.getUserMedia ||
+                      navigator.mediaDevices.mozGetUserMedia ||
+                      navigator.mediaDevices.msGetUserMedia ||
+                      navigator.mediaDevices.webkitGetUserMedia);
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		//let supported = navigator.mediaDevices.getSupportedConstraints();
+		//console.log(supported);
+		video_show(stream);//only show locally, not remotely
+		//recordingCircle.style.fill='red';
+		//socket.emit('config_rtmpDestination', url);
+		socket.emit('start','start');
+		mediaRecorder = new MediaRecorder(stream);
+		mediaRecorder.start(5000);
+
+		//show remote stream
+		var livestream = document.getElementsByClassName("Livestream");
+		console.log("adding live stream");
+		livestream.innerHtml = "test";
+
+		mediaRecorder.onstop = function(e) {
+			console.log("stopped!");
+			console.log(e);
+			//stream.stop();
+				
+		}
+		
+		mediaRecorder.onpause = function(e) {
+			console.log("media recorder paused!!");
+			console.log(e);
+			//stream.stop();
+				
+		}
+		
+		mediaRecorder.onerror = function(event) {
+			let error = event.error;
+			console.log("error", error.name);
+
+  	  };	
+		
+		mediaRecorder.ondataavailable = function(e) {
+		//  console.log("ondataavailable");
+		  socket.emit("binarystream",e.data);
+		  state="start";
+		  //chunks.push(e.data);
+		}
+	}).catch(function(err) {
+		//console.log('The following error occured: ' + err);
+		show_output('Local getUserMedia ERROR:'+err);
+		console.log('Local getUserMedia ERROR:'+err);
+		output_message.innerHTML="Local video source size is not support or No camera ?"+width+"x"+height;
+		 state="stop";
+		
+	});
+}
+
+
+function stopStream(){
+	  	console.log("stop pressed:");
+	  	//stream.getTracks().forEach(track => track.stop())
+	  	mediaRecorder.stop();
+	 // 	recordingCircle.style.fill='gray';
+
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 
-//file system - we need this to delete files after they are uploaded.
-var fs = require('fs');
-//apivideo
-const apiVideo = require('@api.video/nodejs-sdk');
-//set up api.video client with my production key
-//I keep my key in a .env file to keep it private.
-//if you have a .env file, make sure you add it to your .gitignore file
-var client = new apiVideo.Client({ apiKey: process.env.apivideoKeyProd});
-
-
 //express for the website and pug to create the pages
 const app = express();
 const pug = require('pug');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var rtmpEndpoint;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','pug');
-app.use(express.static('public/'));
+app.use(express.static('public'));
 //favicons are the cool little icon in the browser tab
 var favicon = require('serve-favicon');
 app.use(favicon('public/icon.ico')); 
@@ -34,11 +27,88 @@ var validator = require("email-validator");
     let startUploadTimer;
 	let uploadCompleteTimer;
 	let playReadyTimer;
+	
+	
+//file system - we need this to delete files after they are uploaded.
+var fs = require('fs');
+//apivideo
+const apiVideo = require('@api.video/nodejs-sdk');
+//set up api.video client with my production key
+//I keep my key in a .env file to keep it private.
+//if you have a .env file, make sure you add it to your .gitignore file
+var client = new apiVideo.Client({ apiKey: process.env.apivideoKeyProd});
+
+
+
+
+
 
 //get request is the initial request - loads the start.pug
 //start.pug has the form
 app.get('/', (req, res) => {
-  return res.render('start');
+	console.log("get index loaded");
+	var live = req.query.live;
+	if(live){
+		//we have to add a livestream!
+		console.log("live!");
+		
+		//live & socket stuff
+		var spawn = require('child_process').spawn;
+		const server = require('http').createServer(app);
+		var io = require('socket.io')(server);
+		spawn('ffmpeg',['-h']).on('error',function(m){
+			console.error("FFMpeg not found in system cli; please install ffmpeg properly or make a softlink to ./!");
+			process.exit(-1);
+		});
+		
+		//TODO live stuff
+		//i need to set up a  RTMP server from the list
+		//list of streams from api.video
+		let streamList = client.lives.search();
+		
+		streamList.then(function(streams) {
+			
+			let streamCount = streams.length;
+			var streamKey;
+			var streamId;
+			//pick a random stream, and make sure it is not broadcasting
+			var counter =0;
+			let chosenStream = streamPicker(streams,counter);
+			console.log("stream chosen",streams[chosenStream]);
+			if (chosenStream <0){
+				//TODO
+				//Error message required
+			}else{
+				//valid stream
+				
+				streamKey = streams[chosenStream].streamKey;
+				streamId = streams[chosenStream].liveStreamId;
+				rtmpEndpoint = "rtmp://broadcast.api.video/s/"+streamKey;
+				console.log("rtmp endpoint",rtmpEndpoint );
+			}
+			//we've esablished the stream, 
+			var streamUrl = "https://embed.api.video/live/" + streamId;
+			//var iframecode = "iframe src='"+player+"#autoplay'  width = '100%' frameborder='0' scrolling='no'";
+				
+			var iframecode = "iframe src='"+streamUrl+"' controls width='100%' frameborder='0' scrolling='no'";
+			var videoResponse = "live response here";
+			return res.render('index', {iframecode, videoResponse, rtmpEndpoint});
+			
+		});
+		
+		
+		
+		
+	}else{
+	
+		var iframecode = "img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1810676/video_placeholder.png' width='100%'";
+		var videoResponse = "When you upload a video, the API response will appear here."
+		//not live..just loading the page
+		
+		return res.render('index', {iframecode, videoResponse});
+	}
+	
+  
 });
 
 //the form posts the data to the same location
@@ -47,55 +117,29 @@ app.post('/', (req,res) =>{
 	
     //formidable reads the form
 	var form = new formidable.IncomingForm({maxFileSize : 2000 * 1024 * 1024}); //2 Gb
+	//console.log("form",form);
 	//use .env feil to set the directory for the video uploads
 	//since we will be deleting the files after they uplaod to api.video
 	//make sure this directory is full write and delete
 	form.uploadDir = process.env.videoDir;
-	
+    
 	//TODO form validation (mp4 file type, etc)
 	form.parse(req, (err, fields, files) => {
     if (err) {
-		console.error('Error', err);
-		throw err;
+		console.log(err);
+		next(err);
+		return;
     }
 	//testing - writing fields and info on the file to the log
-    console.log('Fields', fields);
-    //console.log('Files', files.source);
-	//console.log('file size', files.source.size);
-	//console.log('file path', files.source.path);
+   // console.log('Fields', fields);
+  //  console.log('Files', files.source);
 	
-	//mp4 support must be boolean - so set to false
-	//then modify to true if the form has it set to true
-	let mp4Support = false;
-	if (fields.mp4 =="true"){
-		mp4Support = true;	
-	}
-	let description = fields.description;
-	let videoTitle = fields.title;
-	let videoTags = fields.tag;
-	//if email address is added use prod
-	let useProduction = true;
-	/*
-	console.log('valid email?', fields.email, validator.validate(fields.email));
-	if(validator.validate(fields.email)){
-		//prod
-        console.log("using production!");
-		client = new apiVideo.Client({ apiKey: process.env.apivideoKeyProd});
-		description = "video uploaded by: "+fields.email;
-	}else{
-		//sandbox
-		//set up api.video client with my sandbox key
-		client = new apiVideo.ClientSandbox({ apiKey: process.env.apivideoKeySandBox});
-		console.log("using sandbox!");
-		useProduction = false;
-	}
-	*/
-	//metadata must be converted into an array
-	
+	var date = new Date();
+	var videoTitle = date.getTime();
 	//uploading.  Timers are for a TODO measuring upload & parsing time
 	startUploadTimer = Date.now();
 	console.log("start upload", startUploadTimer);
-	let result = client.videos.upload(files.source.path, {title: videoTitle, mp4Support: mp4Support, description: description});
+	let result = client.videos.upload(files.source.path, {title: videoTitle});
 	
 	//the result is the upload response
 	//see https://docs.api.video/5.1/videos/create-video
@@ -103,33 +147,25 @@ app.post('/', (req,res) =>{
 	result.then(function(video) {
 		uploadCompleteTimer = Date.now();
 		console.log("upload complete", uploadCompleteTimer);
+		//console.log("video",video);
+		var videoJson = JSON.stringify(video);
 	   //delete file on node server
 		fs.unlink(files.source.path, function (err) {
     	if (err) throw err;
     	// if no error, file has been deleted successfully
     	console.log('File deleted!');
 		}); 
-		
-	  //read information from API.video	upload JSON response
-	  console.log('video', video);
-	  let videoId = video.videoId;
-	  console.log('videoId', videoId);
-	  console.log('player', video.assets.player);
-	  let iframe = video.assets.iframe;
-	  let player = video.assets.player;
-	  let mp4 = "";
-	  
-	  
-
+      //video is uploaded, but not yet published.	
 	  //check video status until it is published
-	  //when video is playable resturn the video page
+	  //when video is playable return the video page
 	  videoStatus(video);
 		 //this means that the video is now playable
 		  //so load video.pug, to display the video to the user.
 	  function videoStatus(video) {
-	  	//console.log(video);
+	  	//get info about video
 	  	let videoId = video.videoId;
 	  	let iframe  = video.assets.iframe;
+		let player = video.assets.player;
 	  	let playable = false;
 	  	let status = client.videos.getStatus(videoId);
 	      status.then(function(videoStats){
@@ -146,20 +182,16 @@ app.post('/', (req,res) =>{
 				let processSeconds = (playReadyTimer - uploadCompleteTimer)/1000;
 				console.log("video uploaded in: ", uploadSeconds);
 				console.log("video processed in: ", processSeconds);
-	  			console.log(iframe);
-		        //now we can get the MP4 url, and send the email and post the response
+	  			//now we can get the MP4 url, and send the email and post the response
 				//now we add the tags to let zapier know it s ready to go
-				client.videos.update(videoId, {tags: [videoTags]});
 				
+				var videoResponse = "uploaded: "+ uploadSeconds+"s \r\n processed: " + processSeconds +"s \n"+videoJson;
+				console.log("videoResponse", videoResponse);
+				var iframecode = "iframe src='"+player+"#autoplay'  width = '100%' frameborder='0' scrolling='no'";
+				console.log(iframecode);
 				
-				let videoMp4 = client.videos.get(videoId);
-				videoMp4.then(function(mp4Stats) {
-		   	 		console.log("videomp4status",mp4Stats);
-		   			mp4 = mp4Stats.assets.mp4;
-		   			console.log("mp4 url", mp4);
-
-	  				return res.render('video', {iframe, player, uploadSeconds,processSeconds});
-		   	 	}); 	
+	  		   return res.render('index', {iframecode, videoResponse});
+		   	 		
 	  		}else{
 	  			//not ready so check again in 2 seconds.
 	  			console.log("not ready yet" );
@@ -188,9 +220,201 @@ app.post('/', (req,res) =>{
 });
 });
 
+//streaming stuff
+var spawn = require('child_process').spawn;
+
+spawn('ffmpeg',['-h']).on('error',function(m){
+
+	console.error("FFMpeg not found in system cli; please install ffmpeg properly or make a softlink to ./!");
+	process.exit(-1);
+});
+io.on('connection', function(socket){
+	console.log("connection");
+	socket.emit('message','Hello from mediarecorder-to-rtmp server!');
+	socket.emit('message','Please set rtmp destination before start streaming.');
+	console.log('message','Hello from mediarecorder-to-rtmp server!');
+	console.log('message','Please set rtmp destination before start streaming.');
+	
+	
+	
+	var ffmpeg_process, feedStream=false;
+
+/*	
+	socket.on('config_rtmpDestination',function(m){
+		if(typeof m != 'string'){
+			socket.emit('fatal','rtmp destination setup error.');
+			return;
+		}
+		var regexValidator=/^rtmp:\/\/[^\s]*$/;//TODO: should read config
+		if(!regexValidator.test(m)){
+			socket.emit('fatal','rtmp address rejected.');
+			return;
+		}
+		rtmpEndpoint=m;
+		console.log('message','rtmp destination set to:'+m);
+		socket.emit('message','rtmp destination set to:'+m);
+	}); 
+*/	
+
+	//socket._vcodec='libvpx';//from firefox default encoder
+	socket.on('config_vcodec',function(m){
+		if(typeof m != 'string'){
+			socket.emit('fatal','input codec setup error.');
+			
+			return;
+		}
+		if(!/^[0-9a-z]{2,}$/.test(m)){
+			socket.emit('fatal','input codec contains illegal character?.');
+			return;
+		}//for safety
+		socket._vcodec=m;
+	}); 	
+
+
+	socket.on('start',function(m){
+		if(ffmpeg_process || feedStream){
+			
+			socket.emit('fatal','stream already started.');
+			console.log('fatal','stream already started.');
+			return;
+		}
+		if(!rtmpEndpoint){
+			socket.emit('fatal','no destination given.');
+			console.log('fatal','no destination given.');
+			return;
+		}
+		
+		var framerate = parseInt(socket.handshake.query.framespersecond);
+		var audioBitrate = parseInt(socket.handshake.query.audioBitrate);
+	    var audioEncoding = "64k";
+		if (audioBitrate ==11025){
+			audioEncoding = "11k";
+		} else if (audioBitrate ==22050){
+			audioEncoding = "22k";
+		} else if (audioBitrate ==44100){
+			audioEncoding = "44k";
+		}
+		console.log("audio numbers" , audioEncoding, audioBitrate);
+		console.log("apivideo server", rtmpEndpoint);
+		//default keyint is 250. but we want 2 for 1fps
+		var key = Math.min(250, framerate*2);
+		var keyint = "keyint="+key;
+		//keyint_min default is 25
+		var keyint_min = Math.min(25, framerate*2);
+			var ops = [
+				'-i','-',
+				 '-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency', 
+				'-max_muxing_queue_size', '1000', 
+				'-bufsize', '5000',
+				//'-r', '1', '-g', '2', '-keyint_min','2', 
+			       '-r', framerate, '-g', framerate*2, '-keyint_min',keyint_min, 
+					'-x264opts',keyint, '-crf', '25', '-pix_fmt', 'yuv420p',
+			        '-profile:v', 'baseline', '-level', '3', 
+     				'-c:a', 'aac', '-b:a',audioEncoding, '-ar', audioBitrate, 
+			        '-f', 'flv', rtmpEndpoint		
+		
+		];
+	
+	    console.log("ops", ops);
+		console.log("rtmp endpoint", rtmpEndpoint);
+		ffmpeg_process=spawn('ffmpeg', ops);
+		console.log("ffmpeg spawned");
+		feedStream=function(data){
+			
+			ffmpeg_process.stdin.write(data);
+			//write exception cannot be caught here.	
+		}
+
+		ffmpeg_process.stderr.on('data',function(d){
+			socket.emit('ffmpeg_stderr',''+d);
+			console.log('ffmpeg_stderr',''+d);
+		});
+		ffmpeg_process.on('error',function(e){
+			console.log('child process error'+e);
+			socket.emit('fatal','ffmpeg error!'+e);
+			feedStream=false;
+			socket.disconnect();
+		});
+		ffmpeg_process.on('exit',function(e){
+			console.log('child process exit'+e);
+			socket.emit('fatal','ffmpeg exit!'+e);
+			socket.disconnect();
+		});
+	});
+
+	socket.on('binarystream',function(m){
+		if(!feedStream){
+			socket.emit('fatal','rtmp not set yet.');
+			console.log('fatal','rtmp not set yet.');
+			ffmpeg_process.stdin.end();
+			ffmpeg_process.kill('SIGINT');
+			return;
+		}
+		feedStream(m);
+	});
+	socket.on('disconnect', function () {
+		console.log("socket disconected!");
+		feedStream=false;
+		if(ffmpeg_process)
+		try{
+			ffmpeg_process.stdin.end();
+			ffmpeg_process.kill('SIGINT');
+			console.log("ffmpeg process ended!");
+		}catch(e){console.warn('killing ffmpeg process attempt failed...');}
+	});
+	socket.on('error',function(e){
+		
+		console.log('socket.io error:'+e);
+	});
+});
+
+io.on('error',function(e){
+	console.log('socket.io error:'+e);
+});
 
 
 
-app.listen(3000, () =>
+
+//streaming stuff
+
+
+
+
+
+
+server.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    console.log(err)
+    // Note: after client disconnect, the subprocess will cause an Error EPIPE, which can only be caught this way.
+})
+
+function streamPicker(streams, counter){
+	console.log(counter);
+	var chosenStream;
+	let streamCount = streams.length;
+	console.log(streamCount);
+	let randomNumber = Math.floor(Math.random()*streamCount);
+	console.log("streampicking", streams[randomNumber]);
+	if(!streams[randomNumber].broadcasting){
+		//the chosen stream is NOT broadcasting
+		chosenStream = randomNumber;
+	}else{
+		//chosen stream IS broadcasting
+		//pick another
+		//but not to infinity - otherwise the demo will break!
+		counter++;
+		if (counter < streamCount*2){
+			streamPicker(streams, counter);	
+		}
+		else{
+			chosenStream = -1;
+		}
+	}
+	return chosenStream;
+	
+}
+
+
